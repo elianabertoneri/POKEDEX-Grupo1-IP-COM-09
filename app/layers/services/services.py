843810fa28e404441a0425fa8aa598ed554c6f44
Eliana Bertoneri <bertonerieli@gmail.com>
2025-06-23 16:55:41 -0300

    Permite ver las imágenes

--- app/layers/services/services.py
+++ app/layers/services/services.py
@@ -12,7 +12,15 @@ def getAllImages():
     # 1) traer un listado de imágenes crudas desde la API (ver transport.py)
     # 2) convertir cada img. en una card.
     # 3) añadirlas a un nuevo listado que, finalmente, se retornará con todas las card encontradas.
-    pass
+
+    #Punto 2 --------------
+    lista_img=transport.getAllImages()
+    lista_cards=[]
+    for elemento in lista_img:
+        card=translator.fromRequestIntoCard(elemento)
+        lista_cards.append(card)
+    return lista_cards
+    
 
 # función que filtra según el nombre del pokemon.
 def filterByCharacter(name):
