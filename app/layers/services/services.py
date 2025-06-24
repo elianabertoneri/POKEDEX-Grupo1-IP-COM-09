540e7200e1e251ecc1125e189c9a150f795e19a0
elianabertoneri <bertonerieli@gmail.com>
2025-06-24 10:42:13 -0300

    Arreglo bordes y fondo card. Agregue funcion de busqueda segun tipo

--- app/layers/services/services.py
+++ app/layers/services/services.py
@@ -39,10 +39,13 @@ def filterByCharacter(name):
 # función que filtra las cards según su tipo.
 def filterByType(type_filter):
     filtered_cards = []
+    type_filter=type_filter.lower()
 
     for card in getAllImages():
+        if type_filter in card.types:
+            filtered_cards.append(card)
+            
         # debe verificar si la casa de la card coincide con la recibida por parámetro. Si es así, se añade al listado de filtered_cards.
-        filtered_cards.append(card)
 
     return filtered_cards
 
